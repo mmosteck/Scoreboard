fcaf1930894c93579ef124d442f1146da2caa073
verat <verat@o2.pl>
2025-01-15 21:44:46 +0100

    Add logic to start a new game

--- src/main/java/main/Scoreboard.java
+++ src/main/java/main/Scoreboard.java
@@ -1,10 +1,21 @@
 package main;
 
 import model.Game;
+import model.Team;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Scoreboard {
 
-    private final ArrayList<Game> gameList = new ArrayList<>();
+    private final Set<Game> gameList = new HashSet<>();
+
+    public void startGame(Team homeTeam, Team awayTeam) {
+        var newGame = new Game(homeTeam, awayTeam);
+        gameList.add(newGame);
+    }
+
+    public int getNumberOfRunningGames() {
+        return gameList.size();
+    }
 }
--- src/test/java/main/ScoreboardTest.java
+++ src/test/java/main/ScoreboardTest.java
@@ -1,17 +1,39 @@
 package main;
 
+import model.Team;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreboardTest {
 
+    Team team1;
+    Team team2;
+
+    @BeforeEach
+    void setUpData() {
+       team1 = new Team("Germany");
+       team2 = new Team("Brazil");
+    }
+
     @Test
     void shouldStartGame() {
+        var scoreboard = new Scoreboard();
 
+        scoreboard.startGame(team1, team2);
+
+        assertEquals(1, scoreboard.getNumberOfRunningGames());
     }
 
     @Test
     void shouldNotStartAlreadyStartedGame() {
+        var scoreboard = new Scoreboard();
 
+        scoreboard.startGame(team1, team2);
+        scoreboard.startGame(team1, team2);
+
+        assertEquals(1, scoreboard.getNumberOfRunningGames());
     }
 
     @Test
