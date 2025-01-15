58a01df89a2384c0ab2e08f17e34e108160cf530
verat <verat@o2.pl>
2025-01-15 21:32:05 +0100

    Add basic test cases

---
+++ src/test/java/main/ScoreboardTest.java
@@ -0,0 +1,51 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+
+public class ScoreboardTest {
+
+    @Test
+    void shouldStartGame() {
+
+    }
+
+    @Test
+    void shouldNotStartAlreadyStartedGame() {
+
+    }
+
+    @Test
+    void shouldFinishGame() {
+
+    }
+
+    @Test
+    void shouldNotFinishFinishedGame() {
+
+    }
+
+    @Test
+    void shouldUpdateScore_HomeTeam() {
+
+    }
+
+    @Test
+    void shouldUpdateScore_AwayTeam() {
+
+    }
+
+    @Test
+    void shouldDisplaySummary() {
+
+    }
+
+    @Test
+    void shouldDisplaySummary_ProperSorting() {
+
+    }
+
+    @Test
+    void shouldDisplaySummary_EmptyScoreboard() {
+
+    }
+}
